f897d047ba714f266efae1f582e0d8f0c867eb4e
Gareth Healy <garethahealy@gmail.com>
2015-01-07 21:24:38 +0000

    Added some fabric notes

---
+++ lb-core/src/main/java/com/garethahealy/camel/file/loadbalancer/fabric/FabricLoadBalancedFileComponent.java
@@ -0,0 +1,27 @@
+/*
+ * #%L
+ * lb-core
+ * %%
+ * Copyright (C) 2013 - 2015 Gareth Healy
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package com.garethahealy.camel.file.loadbalancer.fabric;
+
+public class FabricLoadBalancedFileComponent { // extends MasterComponent
+
+    //https://github.com/fabric8io/fabric8/tree/a5ae951c88f2f4119e35ac505025861ef3275cbd/sandbox/fabric/fabric-camel/src/main/java/io/fabric8/camel
+    //https://access.redhat.com/documentation/en-US/Red_Hat_JBoss_Fuse/6.1/html/Apache_Camel_Component_Reference/Master.html
+    //<reference id="curator" interface="org.apache.curator.framework.CuratorFramework"/>
+}
