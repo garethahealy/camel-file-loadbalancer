12a6fe84e737d44b561e164359ff7b50d476ec02
Gareth Healy <garethahealy@gmail.com>
2015-02-12 13:29:04 +0000

    Updated test to use the result wait time and assert period on the mocks - means we don't need to do a fake sleep

--- lb-examples/lb-example1/src/test/java/com/garethahealy/camel/file/loadbalancer/example1/routes/HandlesOneFileMultipleReadersTest.java
+++ lb-examples/lb-example1/src/test/java/com/garethahealy/camel/file/loadbalancer/example1/routes/HandlesOneFileMultipleReadersTest.java
@@ -24,9 +24,11 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Collection;
 import java.util.Dictionary;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.model.RouteDefinition;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.filefilter.FileFilterUtils;
 import org.junit.Assert;
@@ -60,33 +62,37 @@ public class HandlesOneFileMultipleReadersTest extends BaseCamelBlueprintTestSup
         FileUtils.copyFileToDirectory(FileUtils.toFile(file1), directory);
     }
 
-    private void sleep() {
-        try {
-            TimeUnit.SECONDS.sleep(5);
-        } catch (InterruptedException ex) {
-            //ignore
-        }
-    }
-
     @Test
     public void handlesOneFileMultipleReaders() throws InterruptedException, MalformedURLException {
         MockEndpoint first = getMockEndpoint("mock:endFirst");
         first.setExpectedMessageCount(1);
         first.expectedBodiesReceived("afile1.log");
+        first.setResultWaitTime(TimeUnit.SECONDS.toMillis(15));
+        first.setAssertPeriod(TimeUnit.SECONDS.toMillis(1));
 
         MockEndpoint second = getMockEndpoint("mock:endSecond");
         second.setExpectedMessageCount(0);
+        second.setResultWaitTime(TimeUnit.SECONDS.toMillis(15));
+        second.setAssertPeriod(TimeUnit.SECONDS.toMillis(1));
 
         MockEndpoint third = getMockEndpoint("mock:endThird");
         third.setExpectedMessageCount(0);
-
-        sleep();
+        third.setResultWaitTime(TimeUnit.SECONDS.toMillis(15));
+        third.setAssertPeriod(TimeUnit.SECONDS.toMillis(1));
 
         first.assertIsSatisfied();
         second.assertIsSatisfied();
         third.assertIsSatisfied();
 
-        Collection<File> firstFiles = FileUtils.listFiles(FileUtils.toFile(new URL("file:" + rootDirectory + "/.camel0")), FileFilterUtils.prefixFileFilter("afile1.log"), null);
+        File firstDirectory = FileUtils.toFile(new URL("file:" + rootDirectory + "/.camel0"));
+        File secondDirectory = FileUtils.toFile(new URL("file:" + rootDirectory + "/.camel1"));
+        File thirdDirectory = FileUtils.toFile(new URL("file:" + rootDirectory + "/.camel2"));
+
+        Assert.assertTrue(firstDirectory.exists());
+        Assert.assertFalse(secondDirectory.exists());
+        Assert.assertFalse(thirdDirectory.exists());
+
+        Collection<File> firstFiles = FileUtils.listFiles(firstDirectory, FileFilterUtils.prefixFileFilter("afile1.log"), null);
 
         Assert.assertNotNull(firstFiles);
         Assert.assertFalse(FileUtils.toFile(new URL("file:" + rootDirectory + "/.camel1")).exists());
