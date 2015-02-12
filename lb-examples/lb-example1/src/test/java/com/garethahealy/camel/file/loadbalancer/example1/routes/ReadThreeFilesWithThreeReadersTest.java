2c901b13db9d1a3ae7e8ca01e96c558a8b200bc9
Gareth Healy <garethahealy@gmail.com>
2015-02-12 13:29:56 +0000

    Added method to log out what the priorities for the endpoints under test are Updated tests to check few more things

--- lb-examples/lb-example1/src/test/java/com/garethahealy/camel/file/loadbalancer/example1/routes/ReadThreeFilesWithThreeReadersTest.java
+++ lb-examples/lb-example1/src/test/java/com/garethahealy/camel/file/loadbalancer/example1/routes/ReadThreeFilesWithThreeReadersTest.java
@@ -24,15 +24,29 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Collection;
 import java.util.Dictionary;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
+import com.garethahealy.camel.file.loadbalancer.core.LoadBalancedFileEndpoint;
+import com.garethahealy.camel.file.loadbalancer.filter.PriorityFileFilter;
+
+import org.apache.camel.Endpoint;
+import org.apache.camel.component.file.GenericFileFilter;
 import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.model.FromDefinition;
+import org.apache.camel.model.RouteDefinition;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.filefilter.FileFilterUtils;
 import org.junit.Assert;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ReadThreeFilesWithThreeReadersTest extends BaseCamelBlueprintTestSupport {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ReadThreeFilesWithThreeReadersTest.class);
 
     private String rootDirectory = System.getProperty("user.dir") + "/target/files";
 
@@ -66,6 +80,46 @@ public class ReadThreeFilesWithThreeReadersTest extends BaseCamelBlueprintTestSu
         FileUtils.copyFileToDirectory(FileUtils.toFile(file3), directory);
     }
 
+    /**
+     * Get a map with the route id (key) and the priority (value) for 'from' endpoint
+     *
+     * @return
+     */
+    private Map<String, String> getRouteToEndpointPriority() {
+        Map<String, String> answer = new HashMap<String, String>();
+
+        List<RouteDefinition> routes = context.getRouteDefinitions();
+
+        Assert.assertNotNull(routes);
+
+        for (RouteDefinition current : routes) {
+            Assert.assertEquals(new Integer(1), new Integer(current.getInputs().size()));
+            Assert.assertNotNull(current.getInputs().get(0));
+            Assert.assertNotNull(current.getId());
+            Assert.assertTrue(current.getId().trim().length() > 0);
+
+            FromDefinition from = current.getInputs().get(0);
+            Endpoint endpoint = getMandatoryEndpoint(from.getUri());
+
+            Assert.assertNotNull(endpoint);
+            Assert.assertTrue(endpoint instanceof LoadBalancedFileEndpoint);
+
+            LoadBalancedFileEndpoint lbEndpoint = (LoadBalancedFileEndpoint)endpoint;
+            GenericFileFilter filter = lbEndpoint.getFilter();
+
+            Assert.assertNotNull(filter);
+            Assert.assertTrue(filter instanceof PriorityFileFilter);
+
+            PriorityFileFilter priorityFileFilter = (PriorityFileFilter)filter;
+
+            Assert.assertNotNull(priorityFileFilter.getPriorityName());
+
+            answer.put(current.getId(), priorityFileFilter.getPriorityName());
+        }
+
+        return answer;
+    }
+
     private void sleep() {
         try {
             TimeUnit.SECONDS.sleep(10);
@@ -76,24 +130,40 @@ public class ReadThreeFilesWithThreeReadersTest extends BaseCamelBlueprintTestSu
 
     @Test
     public void readThreeFilesWithThreeReaders() throws InterruptedException, MalformedURLException {
+        Map<String, String> answer = getRouteToEndpointPriority();
+
+        //Used for debugging purposes, in-case we need to know which endpoint has what priority
+        LOG.info("EndpointSetup: " + answer.toString());
+
         MockEndpoint first = getMockEndpoint("mock:endFirst");
         first.setExpectedMessageCount(1);
+        first.setResultWaitTime(TimeUnit.SECONDS.toMillis(15));
+        first.setAssertPeriod(TimeUnit.SECONDS.toMillis(1));
 
         MockEndpoint second = getMockEndpoint("mock:endSecond");
         second.setExpectedMessageCount(1);
+        second.setResultWaitTime(TimeUnit.SECONDS.toMillis(15));
+        second.setAssertPeriod(TimeUnit.SECONDS.toMillis(1));
 
         MockEndpoint third = getMockEndpoint("mock:endThird");
         third.setExpectedMessageCount(1);
+        third.setResultWaitTime(TimeUnit.SECONDS.toMillis(15));
+        third.setAssertPeriod(TimeUnit.SECONDS.toMillis(1));
 
+        //Wait for the files to be processed
         sleep();
 
-        first.assertIsSatisfied();
-        second.assertIsSatisfied();
-        third.assertIsSatisfied();
+        File firstDirectory = FileUtils.toFile(new URL("file:" + rootDirectory + "/.camel0"));
+        File secondDirectory = FileUtils.toFile(new URL("file:" + rootDirectory + "/.camel1"));
+        File thirdDirectory = FileUtils.toFile(new URL("file:" + rootDirectory + "/.camel2"));
 
-        Collection<File> firstFiles = FileUtils.listFiles(FileUtils.toFile(new URL("file:" + rootDirectory + "/.camel0")), FileFilterUtils.fileFileFilter(), null);
-        Collection<File> secondFiles = FileUtils.listFiles(FileUtils.toFile(new URL("file:" + rootDirectory + "/.camel1")), FileFilterUtils.fileFileFilter(), null);
-        Collection<File> thirdFiles = FileUtils.listFiles(FileUtils.toFile(new URL("file:" + rootDirectory + "/.camel2")), FileFilterUtils.fileFileFilter(), null);
+        Assert.assertTrue(firstDirectory.exists());
+        Assert.assertTrue(secondDirectory.exists());
+        Assert.assertTrue(thirdDirectory.exists());
+
+        Collection<File> firstFiles = FileUtils.listFiles(firstDirectory, FileFilterUtils.fileFileFilter(), null);
+        Collection<File> secondFiles = FileUtils.listFiles(secondDirectory, FileFilterUtils.fileFileFilter(), null);
+        Collection<File> thirdFiles = FileUtils.listFiles(thirdDirectory, FileFilterUtils.fileFileFilter(), null);
 
         Assert.assertNotNull(firstFiles);
         Assert.assertNotNull(secondFiles);
@@ -102,5 +172,11 @@ public class ReadThreeFilesWithThreeReadersTest extends BaseCamelBlueprintTestSu
         Assert.assertEquals(new Integer(1), new Integer(firstFiles.size()));
         Assert.assertEquals(new Integer(1), new Integer(secondFiles.size()));
         Assert.assertEquals(new Integer(1), new Integer(thirdFiles.size()));
+
+        //Assert the endpoints last, as there seems to be a strange bug where they fail but the files have been processed,
+        //so that would suggest the MockEndpoints are reporting a false-positive
+        first.assertIsSatisfied();
+        second.assertIsSatisfied();
+        third.assertIsSatisfied();
     }
 }
